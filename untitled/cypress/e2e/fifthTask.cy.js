27c464d996c9b4a24bac7897d979bb75124f4376
osoroka <olena.soroka@wikrgroup.com>
2022-12-01 13:09:51 +0100

    chnage accessToken

--- untitled/cypress/e2e/fifthTask.cy.js
+++ untitled/cypress/e2e/fifthTask.cy.js
@@ -1,14 +1,15 @@
-import {accessToken} from '../user.json'
+import body from "../fixtures/user";
 
 describe('fifth Task', () => {
     it('Create post with adding access token in header', () => {
+        let accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InFhX3Rlc3QxMjM0NTY3ODlAbWFpbC5jb20iLCJpYXQiOjE2Njk4OTMzMzUsImV4cCI6MTY2OTg5NjkzNSwic3ViIjoiMTMifQ.MTxx8cBhN64aIb5z2AEkdv5yKPYFYN9YaVm3_9B3wiM';
         cy.request({
             method: 'POST',
             url: '/664/posts',
             body: body,
             headers: {
                 'Content-Type': 'text/html',
-                'accessToken'  : `${accessToken}`,
+                'accessToken'  : accessToken,
             },
             failOnStatusCode: false
         }).then(response => {
